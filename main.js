40e91fb32909679f16e715e53dc5d2800c2c5d11
jake0102 <jakejapan0102@gmail.com>
2022-03-27 15:01:34 +0900

    new

---
+++ main.js
@@ -0,0 +1,5 @@
+const row = document.querySelector('.firstlook-nav-row')
+const img = document.querySelector('.firstlook-background-img')
+
+console.log(row.scrollHeight);
+console.log(img.scrollHeight);
